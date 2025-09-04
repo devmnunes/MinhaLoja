ef7c90f50413c9cda8a951ed52048fee3c550521
Matheus Nunes <matheus_gustavo97@hotmail.com>
2025-09-03 22:25:42 -0300

    commit

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -10,14 +10,17 @@ class ProductFormPage extends StatefulWidget {
 class _ProductFormPageState extends State<ProductFormPage> {
   final _priceFocus = FocusNode();
   final _descriptionFocus = FocusNode();
+
   final _imageUrlFocus = FocusNode();
   final _imageUrlController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+  final _formData = Map<String, Object>();
 
   @override
   void initState() {
     super.initState();
     _imageUrlFocus.addListener(updateImage);
-
   }
 
   @override
@@ -30,18 +33,32 @@ class _ProductFormPageState extends State<ProductFormPage> {
   }
 
   void updateImage() {
-    setState(() {
-      
-    });
+    setState(() {});
+  }
+
+  void _submitForm() {
+    _formKey.currentState?.save();
+    print(_formData.values);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Cadastro de Produto')),
+      appBar: AppBar(
+        title: Text('Cadastro de Produto'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _submitForm();
+            },
+            icon: Icon(Icons.save),
+          ),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Form(
+          key: _formKey,
           child: ListView(
             children: [
               TextFormField(
@@ -60,6 +77,7 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocus);
                 },
+                onSaved: (nome) => _formData['nome'] = nome ?? '-',
               ),
 
               TextFormField(
@@ -76,12 +94,12 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 ),
                 textInputAction: TextInputAction.next,
                 focusNode: _priceFocus,
-                keyboardType: TextInputType.numberWithOptions(
-                  decimal: true,
-                ),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocus);
                 },
+                onSaved: (preco) =>
+                    _formData['preço'] = double.parse(preco ?? '0'),
               ),
 
               TextFormField(
@@ -99,6 +117,7 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 textInputAction: TextInputAction.next,
                 focusNode: _descriptionFocus,
                 maxLines: 2,
+                onSaved: (descicao) => _formData['descrição'] = descicao ?? '-',
               ),
 
               Row(
@@ -121,28 +140,26 @@ class _ProductFormPageState extends State<ProductFormPage> {
                       focusNode: _imageUrlFocus,
                       controller: _imageUrlController,
                       keyboardType: TextInputType.url,
-                      
+                      onFieldSubmitted: (_) => _submitForm(),
+                      onSaved: (imageUrl) =>
+                          _formData['imageUrl'] = imageUrl ?? '-',
                     ),
                   ),
                   Container(
                     height: 105,
                     width: 105,
-                    margin: EdgeInsets.only(
-                      top: 10,
-                      left: 10,
-                    ),
+                    margin: EdgeInsets.only(top: 10, left: 10),
                     decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 1
-                      )
+                      border: Border.all(color: Colors.grey, width: 1),
                     ),
                     alignment: Alignment.center,
-                    child: _imageUrlController.text.isEmpty ? Text('Informe a Url') : FittedBox(
-                      child: Image.network(_imageUrlController.text),
-                      fit: BoxFit.cover,
-                    )
-                  )
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Informe a Url')
+                        : FittedBox(
+                            child: Image.network(_imageUrlController.text),
+                            fit: BoxFit.cover,
+                          ),
+                  ),
                 ],
               ),
             ],
