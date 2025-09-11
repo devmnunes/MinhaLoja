8f081c4e59222a9c8a6b076ec9c699bec3f7a321
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2025-09-11 14:13:48 -0300

    commit

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:loja/data/dummy_data.dart';
 import 'package:loja/models/product.dart';
 
 class ProductList with ChangeNotifier {
+  final _baseUrl = 'https://shop-cod3r-c1bff-default-rtdb.firebaseio.com';
   final List<Product> _items = dummyProducts;
 
   List<Product> get items => [..._items];
@@ -34,6 +37,20 @@ class ProductList with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    http.post(
+      Uri.parse('$_baseUrl/products.json'),
+      body: jsonEncode(
+        {
+        "name" : product.name,
+        "description" : product.description,
+        "price" : product.price,
+        "imageUrl" : product.imageUrl,
+        "isFavorite" : product.isFavorite,
+      },
+      ),
+    );
+
+
     _items.add(product);
     notifyListeners();
   }
