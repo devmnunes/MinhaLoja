51f201475b468c6ad54d73ebd4f48218d33ef31d
Matheus Nunes <matheus_gustavo97@hotmail.com>
2025-09-27 20:34:40 -0300

    commit

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:loja/components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -10,12 +13,13 @@ class AuthPage extends StatelessWidget {
         children: [
           Container(
             decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Color.fromRGBO(215, 117, 255, 0.5),
-                Color.fromRGBO(255, 188, 117, 0.9)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(215, 117, 255, 0.5),
+                  Color.fromRGBO(255, 188, 117, 0.9),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
             ),
           ),
@@ -26,16 +30,29 @@ class AuthPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                  child: Text('Minha Loja', 
-                  style: TextStyle(
-                    fontSize: 45,
-                    fontFamily: 'Anton',
-                    
-                  ),),
-                )
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 70),
+                  // cascade operator
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrange.shade900,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Colors.black26,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Text(
+                    'Minha Loja',
+                    style: TextStyle(fontSize: 45, fontFamily: 'Anton'),
+                  ),
+                ),
+                AuthForm(),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
